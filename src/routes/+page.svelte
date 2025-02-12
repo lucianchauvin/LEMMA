6f2b9a018547142f7138be1aafbbb5b585186a71
Jared Wang <jwangj001@tamu.edu>
2025-02-11 22:37:29 -0600

    Began working on the front-end aspect of the project, designing the layout of the house and the title of the course canvas page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="home-container">
+  <img src="/House.png" width="50" alt="Canvas Home Sign">
+  <h1 class="home-text">Home Page</h1>
+</div>
+<div class="line"></div>
+
+<style>
+  .home-container {
+    display: flex;
+    align-items: center; /* Aligns items vertically */
+    justify-content: center; /* Centers the content horizontally */
+  }
+
+  .home-text {
+    margin-left: 20px; /* Adds space between image and text */
+  }
+  
+  /* Line below the image and text */
+  .line{
+    width: 100%; 
+    border-bottom: 2px solid black; /* Adds a line */
+    margin-top: 15px; /* Adds space between the image+caption and the line */
+  }
+</style>
