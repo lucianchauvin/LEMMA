0b4395c760c1a1933a4966924f6009bb6d4f7be2
Jared Wang <jwangj001@tamu.edu>
2025-02-16 01:29:14 -0600

    Frontend: homepage - created the icon image for CSCE222 course

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<svg width="100%">
+<svg width="100%" height="1000">
   <!-- Rectangle shape, filling the entire width -->
   <rect x="0" y="0" width="100%" height="100" fill="#0077be" />
   
@@ -16,4 +16,20 @@
   <text x="120"  y="75"  font-size="50"  text-anchor="start" fill="white" alignment-baseline="middle">
     {"Home Page"}
   </text>
+
+  <text x="50%" y="140" font-size="30" text-anchor="middle" fill="black">
+    Courses
+  </text>
+
+  <rect x="43.5%" y="200" width="200" height="40" fill="#008000" stroke="black" stroke-width="3" /> 
+  <text x="50%" y = "230" font-size="25" text-anchor="middle" fill="white">
+    CSCE222
+  </text>
+  <rect x="43.5%" y="240" width="200" height="120" fill="white" stroke="black" stroke-width="3" />
+  <text x="50%" y = "275" font-size="15" text-anchor="middle" fill="w==black">
+    Discrete Structures for
+    <tspan x="50%" dy="20">Computing</tspan>
+  </text>
 </svg>
+
+
