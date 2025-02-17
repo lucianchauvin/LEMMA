f4fdf3c831e81dae89b884b443ae6e4b1e915fae
Jared Wang <jwangj001@tamu.edu>
2025-02-17 16:52:56 -0600

    frontend: added blue rectangle on top of every page, planing to add a house and name of page on the rectangle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,27 @@
-<nav style="border: solid; border-width: 0 0 2px; padding-bottom: 5px;">
-	<a href="/">Home</a>
-	<a href="/about">About</a>
-</nav>
+<script lang="ts">
+    export let children;
+</script>
 
-<slot />
+<style>
+    .rectangle {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100px;
+        background-color: blue;
+    }
+
+    .content {
+        margin-top: 100px; /* Push content below the rectangle */
+    }
+</style>
+
+<!-- Blue rectangle at (0,0) -->
+<div class="rectangle"></div>
+
+<!-- Render child pages below -->
+<div class="content">
+    <slot />
+</div>
+
