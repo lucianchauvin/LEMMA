d53fe0a664958ebe468b20bd33428e4be2081a2b
Lucian Chauvin <lucianchauvin@gmail.com>
2025-02-17 15:32:31 -0600

    CICD: read base64 secret instead

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,16 @@
 import pg from 'pg';
 import { type Handle } from '@sveltejs/kit';
-import { PGHOST, PGUSER, PGPASSWORD, PGPORT } from '$env/static/private';
+import { PGHOST, PGUSER, PGDATABASE, PGPASSWORD, PGPORT } from '$env/static/private';
 
 export const handle: Handle = async ({ event, resolve }) => {
     /**
     * Creates a Postgres client specific to this server request.
     */
+    console.log("'" + Buffer.from(PGPASSWORD, 'base64').toString('utf-8').trim().replace(/(\r\n|\n|\r)/gm, "") + "'");
     event.locals.database = new pg.Pool({
         host: PGHOST,
         user: PGUSER,
-        password: PGPASSWORD,
+        password: Buffer.from(PGPASSWORD, 'base64').toString('utf-8').trim().replace(/(\r\n|\n|\r)/gm, ""),
         port: parseInt(PGPORT)
     })
 
