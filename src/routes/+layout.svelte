49205f0ce0fce7c960861aacf41d20d643de56f3
Jared Wang <jwangj001@tamu.edu>
2025-02-17 22:13:01 -0600

    Remove hardcoding coordinate positions of elements by using flexbox

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,26 +3,89 @@
 </script>
 
 <style>
-    .rectangle {
-        position: absolute;
-        top: 0;
-        left: 0;
+    /* Top Blue Header */
+    .header {
         width: 100%;
         height: 100px;
         background-color: blue;
+        display: flex;
+        align-items: center; /* Centers content vertically */
+        padding: 0 20px;
+    }
+
+    .header img {
+        width: 75px;
+        height: 75px;
     }
 
     .content {
-        margin-top: 100px; /* Push content below the rectangle */
+        margin-top: 20px; /* Adds space below the header */
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
+
+    /* Courses Section */
+    .courses-container {
+        display: flex;
+        justify-content: center; /* Centers horizontally */
+        gap: 50px; /* Adds spacing between courses */
+        margin-top: 20px;
+    }
+
+    .course {
+        width: 200px;
+        height: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: bold;
+        color: white;
+        border: 2px solid black;
+    }
+
+    .dark-green {
+        background-color: darkgreen;
+    }
+
+    .maroon {
+        background-color: maroon;
+    }
+
+    /* Course Descriptions */
+    .course-description {
+        width: 200px;
+        height: 100px;
+        border: 2px solid black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        gap: 0; 
+    }
+
 </style>
 
-<!-- Blue rectangle at (0,0) -->
-<div class="rectangle"></div>
-<img src="/Canvas_Home.png" alt="Home" style="position: absolute; top: 15px; left: 15px; width: 75px; height: 75px;" />
-
-<!-- Render child pages below -->
-<div class="content">
-    <slot />
+<!-- Blue Header -->
+<div class="header">
+    <img src="/Canvas_Home.png" alt="Home">
 </div>
 
+<!-- Page Content -->
+<div class="content">
+    <h1>Courses</h1>
+
+    <div class="courses-container">
+        <!-- Course 1 -->
+        <div>
+            <div class="course dark-green">CSCE222</div>
+            <div class="course-description">Discrete Mathematics</div>
+        </div>
+
+        <!-- Course 2 -->
+        <div>
+            <div class="course maroon">MATH415</div>
+            <div class="course-description">Modern Algebra</div>
+        </div>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,56 +5,72 @@
 <h1 style="text-align: center;">Courses</h1>
 
 <style>
+    .courses-container {
+        display: flex;
+        justify-content: center;
+        gap: 50px; /* Space between courses */
+        margin-top: 20px;
+    }
+
+    .course {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
     .rectangle {
         width: 200px; 
         height: 30px; 
-        border: 2px solid black; /* Add black border with size 2px */
+        border: 2px solid black;
         display: flex;
         justify-content: center;
         align-items: center;
         font-weight: bold;
-        color: white; /* White text*/
+        color: white;
     }
 
     .dark-green {
         background-color: darkgreen;
-        top: 175px; 
-        left: 400px; 
     }
-    
+
     .maroon {
         background-color: maroon;
-        top: 175px;
-        left: 700px; 
     }
 
     .border-rectangle {
         width: 200px;
         height: 100px; 
-        border: 2px solid black; /* Create border lines */
+        border: 2px solid black;
         display: flex;
         justify-content: center;
         align-items: center;
         text-align: center;
+        margin-top: 5px; /* Ensures no large gap */
     }
 
-    .border-dark-green {
-        top: 205px; 
-        left: 400px; 
-        color: black;
-    }
-
-    .border-maroon {
-        top: 205px; 
-        left: 700px; 
-        color: black;
+    .sean-component-container {
+        display: flex;
+        justify-content: center;
+        margin-top: 40px;
     }
 </style>
 
+<!-- Courses Container -->
+<div class="courses-container">
+    <!-- Course 1 -->
+    <div class="course">
+        <div class="rectangle dark-green">CSCE222</div>
+        <div class="border-rectangle">Discrete Mathematics</div>
+    </div>
 
-<div class="rectangle dark-green"> CSCE222 </div> 
-<div class="rectangle maroon"> MATH415 </div> 
+    <!-- Course 2 -->
+    <div class="course">
+        <div class="rectangle maroon">MATH415</div>
+        <div class="border-rectangle">Modern Algebra</div>
+    </div>
+</div>
 
-<!-- Border rectangles below dark green and yellow -->
-<div class="border-rectangle border-dark-green"> Discrete Mathematics </div> 
-<div class="border-rectangle border-maroon"> Modern Algebra </div> 
+<!-- Space for Sean's Component -->
+<div class="sean-component-container">
+    <slot></slot> <!-- This allows Sean's component to be inserted dynamically -->
+</div> 
