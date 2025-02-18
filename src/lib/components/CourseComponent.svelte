aa426a332da77c0d058c7ccdaefe23e56843b466
seanDtamu <sean21tamu@tamu.edu>
2025-02-17 21:03:48 -0600

    created course icon component

---
+++ src/lib/components/CourseComponent.svelte
@@ -0,0 +1,66 @@
+  
+<script>
+  // Course Customization
+  export let color = "white"
+  export let CourseColor = "red";
+  export let CourseNumber = "Course #";
+  export let CourseName = "Course Name";
+
+  // Position
+  export let top = "0px";
+  export let left= "0px";
+  export let margin = "20px";
+</script>
+
+<div class="shape-container" style="margin: {margin}; position: relative; top: {top}; left: {left}">
+  <div class="square-container">
+    <div class="small-rectangle" style="background-color: {CourseColor};">
+      {CourseNumber}
+    </div>
+    <div class="square" style="background-color: {color};">
+      <div class="square-text">
+        {CourseName}
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .shape-container {
+    text-align: center;
+    position: relative;
+  }
+
+  .square-container {
+    display: inline-block;
+    position: relative;
+  }
+
+  .square {
+    width: 200px;
+    height: 140px;
+    display: inline-block;
+    border: 3px solid black;
+    padding-top: 45px;
+  }
+
+  .small-rectangle {
+    width: 200px;
+    height: 45px;
+    border: 3px solid black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 18px;
+    font-weight: bold;
+    color: white;
+  }
+
+  .square-text {
+    text-align: center;
+    font-size: 18px;
+    font-weight: normal;
+    color: black;
+  }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script>
     export let data;
+    import CourseComponent from '$lib/components/CourseComponent.svelte';
 </script>
+
+<a href="/" title="Go to Home" target="_blank">
+    <img class="img-responsive img hidden-xs hidden-sm" id="tamuwhitelogo" style="max-width: 300px" src="home-icon.jpg" alt="House Icon">
+    <img class="img-responsive img visible-xs visible-sm" id="tamuwhitelogo_sm" style="max-height: 50px; padding:0.2em" src="https://cdn.eis.tamu.edu/public/images/TAM-Primary-white.png" alt="TAMU Logo">
+  </a>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
@@ -11,3 +17,7 @@
         <li><a href="/{course_id}">{course_name}</a></li> 
     {/each}
 </ul>
+
+  
+<CourseComponent CourseColor = "green" CourseName = "Discrete Structures for Computing" CourseNumber = "CSCE 222" top = "-100px" left = "-100px" margin = "0px"/>
+  
