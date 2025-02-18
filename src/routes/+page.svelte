7a94e8c6e9238c86ef2304f50d35283c0be3ef12
Jared Wang <jwangj001@tamu.edu>
2025-02-17 18:07:00 -0600

    Front End: Created the general layout of the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,55 @@
     export let data;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<h2>Other pages</h2>
+<h1 style="text-align: center;">Courses</h1>
 <ul>
     {#each data.courses as { course_id, course_name }}
-        <li><a href="/{course_id}">{course_name}</a></li> 
+       <li><a href="/{course_id}">{course_name}</a></li> 
     {/each}
-</ul>
+</ul> 
+
+<style>
+    .rectangle {
+        width: 200px; 
+        height: 30px; 
+        position: absolute; /* Position rectangles relative to the container or viewport */
+        border: 2px solid black; /* Add black border with size 2px */
+    }
+
+    .dark-green {
+        background-color: darkgreen;
+        top: 175px; 
+        left: 400px; 
+    }
+    
+    .yellow {
+        background-color: yellow;
+        top: 175px;
+        left: 700px; 
+    }
+
+    .border-rectangle {
+        width: 200px;
+        height: 100px; 
+        position: absolute;
+        border: 2px solid black; /* Create border lines */
+    }
+
+    .border-dark-green {
+        top: 205px; /* Position below the dark-green rectangle */
+        left: 400px; /* Align with dark-green rectangle */
+    }
+
+    .border-yellow {
+        top: 205px; /* Position below the yellow rectangle */
+        left: 700px; /* Align with yellow rectangle */
+    }
+</style>
+
+
+<div class="rectangle dark-green"></div> <!-- First rectangle at x = 350px, y = 150px -->
+<div class="rectangle yellow"></div> <!-- Second rectangle at x = 100px, y = 150px -->
+
+<!-- Border rectangles below dark green and yellow -->
+<div class="border-rectangle border-dark-green"></div> <!-- Border rectangle below dark green -->
+<div class="border-rectangle border-yellow"></div> <!-- Border rectangle below yellow -->
