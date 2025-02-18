ccf10385874d90d8978a5e12b2d43cd9b4826cf8
Jared Wang <jwangj001@tamu.edu>
2025-02-17 21:30:31 -0600

    Added texts to the course icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,11 @@
         height: 30px; 
         position: absolute; /* Position rectangles relative to the container or viewport */
         border: 2px solid black; /* Add black border with size 2px */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: bold;
+        color: white; /* White text*/
     }
 
     .dark-green {
@@ -18,8 +23,8 @@
         left: 400px; 
     }
     
-    .yellow {
-        background-color: yellow;
+    .maroon {
+        background-color: maroon;
         top: 175px;
         left: 700px; 
     }
@@ -29,23 +34,29 @@
         height: 100px; 
         position: absolute;
         border: 2px solid black; /* Create border lines */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
     }
 
     .border-dark-green {
-        top: 205px; /* Position below the dark-green rectangle */
-        left: 400px; /* Align with dark-green rectangle */
+        top: 205px; 
+        left: 400px; 
+        color: black;
     }
 
-    .border-yellow {
-        top: 205px; /* Position below the yellow rectangle */
-        left: 700px; /* Align with yellow rectangle */
+    .border-maroon {
+        top: 205px; 
+        left: 700px; 
+        color: black;
     }
 </style>
 
 
-<div class="rectangle dark-green"></div> <!-- First rectangle at x = 350px, y = 150px -->
-<div class="rectangle yellow"></div> <!-- Second rectangle at x = 100px, y = 150px -->
+<div class="rectangle dark-green"> CSCE222 </div> 
+<div class="rectangle maroon"> MATH415 </div> 
 
 <!-- Border rectangles below dark green and yellow -->
-<div class="border-rectangle border-dark-green"></div> <!-- Border rectangle below dark green -->
-<div class="border-rectangle border-yellow"></div> <!-- Border rectangle below yellow -->
+<div class="border-rectangle border-dark-green"> Discrete Mathematics </div> 
+<div class="border-rectangle border-maroon"> Modern Algebra </div> 
