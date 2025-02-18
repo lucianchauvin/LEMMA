5ca6533345bcfec5a0133fb240fcbfa9cd6eaedd
Marvin Fung <marvin.fung37@gmail.com>
2025-02-17 20:23:50 -0600

    Adds script to automatically generate sql file to init database

--- data/50_courses.sql
+++ data/50_courses.sql
@@ -1,4 +1,4 @@
 INSERT INTO courses (course_number, course_name, start_date, end_date)
 VALUES
-    ('CSCE222', "Discrete Math", "2025-01-01 12:00:00-06", "2025-12-31 12:00:00-06")
-    ('MATH415', "Modern Algebra", "2025-01-01 12:00:00-06", "2025-12-31 12:00:00-06")
+    ('CSCE222', 'Discrete Math', '2025-01-01 12:00:00-06', '2025-12-31 12:00:00-06'),
+    ('MATH415', 'Modern Algebra', '2025-01-01 12:00:00-06', '2025-12-31 12:00:00-06')
--- data/init_db.sql
+++ data/init_db.sql
@@ -2,8 +2,7 @@ DROP SCHEMA lemma CASCADE;
 CREATE SCHEMA lemma;
 
 \i 00_schema.sql
-\i 01_role_targets.sql
 \i 01_roles.sql
+\i 01_role_targets.sql
 \i 02_role_permissions.sql
 \i 50_courses.sql
-
