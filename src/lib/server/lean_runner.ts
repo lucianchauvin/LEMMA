4c6b8af83c9e193c2b8857a16ef078ceafcae101
Andrew800 <andrewzhengschool@gmail.com>
2025-02-18 09:00:05 -0600

    Changed to use new .env file variable

--- src/lib/server/lean_runner.ts
+++ src/lib/server/lean_runner.ts
@@ -1,14 +1,14 @@
+import { LEANBIN } from "$env/static/private"
 import { exec } from "child_process";
 
 // Unsure how Lean file will be passed into the function, assuming a string (file path) will be given
 // Puts output to console.log for now
 // Does not connect to database for now (unsure if this file will even handle that)
-async function run(file)
+async function run(file: string)
 {
     return new Promise(function (resolve, reject)
     {
-        //const path = "C:/Users/andre/.elan/bin/lean.exe --run "
-        const path = "/home/ubuntu/.elan/bin/lean --run "
+        const path = LEANBIN.replaceAll("\\", "/") + "/lean --run ";
         exec(path + file, (err, stdout, stderr) =>
         {
             if (err)
@@ -25,11 +25,11 @@ async function run(file)
     });
 }
 
-/*
+
 async function main()
 {
     let { stdout } = await run("../../test.lean");
 }
 
 main();
-*/
+
