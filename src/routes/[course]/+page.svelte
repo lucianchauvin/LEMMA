a3ddac892a513c87fba482f3645c10222fa3e04b
Jared Wang <jwangj001@tamu.edu>
2025-02-24 23:14:00 -0600

    Reverted back to AppRail bc. teammate says that AppRail is actually the correct tool

--- src/routes/[course]/+page.svelte
+++ src/routes/[course]/+page.svelte
@@ -1,8 +1,9 @@
 <script>
-    import { AppShell } from '@skeletonlabs/skeleton';
+    import {AppRail, AppRailTile} from '@skeletonlabs/skeleton';
 </script>
 
 <style>
+    
     .CourseNavigationTile {
         display: flex;
         justify-content: center;
@@ -11,7 +12,6 @@
         height: 60px;
         background-color: azure;
         border-radius: 8px;
-        cursor: pointer;
     }
 
     .BarsSolid {
@@ -33,12 +33,11 @@
         width: 60px;
         height: 60px;
         background-color: green;
-        margin-top: 100px;
+        margin-top: 100px; 
         display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        cursor: pointer;
+        flex-direction: column;  
+        align-items: center;     
+        justify-content: center; 
     }
 
     .HomeTile img {
@@ -50,50 +49,45 @@
         height: 60px;
         background-color: azure;
         display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        cursor: pointer;
+        flex-direction: column;  
+        align-items: center;     
+        justify-content: center; 
     }
-
 </style>
 
-<AppShell>
-    <div style="display: flex; flex-direction: row; height: 100%;">
-
-        <div style="width: 60px; background-color: #f4f4f4; padding-top: 20px; display: flex; flex-direction: column; align-items: center;">
-            <div class="CourseNavigationTile" on:click={() => alert('Bars clicked')}>
-                <div class="BarsSolid">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
-
-            <div class="HomeTile" on:click={() => alert('Home clicked')}>
-                <img src="/home-icon.svg" alt="House" width="15px" height="15px" style="margin-top: 10px;"> 
-                <h1 style="font-size: 10px;"> Home </h1>
-            </div>
-
-            <div class="OtherTile" on:click={() => alert('Grades clicked')}>
-                <img src="/CheckMark.jpg" alt="Grades" width="20px" height="20px" style="margin-top: 10px;">
-                <h1 style="font-size: 10px;"> Grades </h1>
-            </div>
-
-            <div class="OtherTile" on:click={() => alert('Grade Book clicked')}>
-                <img src="/GradeBook.png" alt="GradeBook" width="20px" height="20px" style="margin-top: 10px;">
-                <h1 style="font-size: 10px;"> Grade Book </h1>
-            </div>
-
-            <div class="OtherTile" on:click={() => alert('Students clicked')}>
-                <img src="/Student.png" alt="Students" width="20px" height="20px" style="margin-top: 10px;">
-                <h1 style="font-size: 10px;"> Students </h1>
-            </div>
-
-            <div class="OtherTile" on:click={() => alert('Statements clicked')}>
-                <img src="/LightBulb.png" alt="Light Bulb Icon" width="20px" height="20px" style="margin-top: 10px;">
-                <h1 style="font-size: 10px;"> Statements </h1>
+<AppRail>
+    <AppRailTile group="" name="" value="">
+        <div class="CourseNavigationTile">
+            <div class="BarsSolid">
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
         </div>
-    </div>
-</AppShell>
+
+        <div class="HomeTile">
+            <img src="/home-icon.svg" alt="House" width=15px height=15px style="margin-top: 10px;"> 
+            <h1 style="font-size: 10px;"> Home </h1>
+        </div>
+
+        <div class = "OtherTile">
+            <img src="/CheckMark.jpg" alt="Grades" width=20px height=20px style="margin-tip: 10px;">
+            <h1 style="font-size: 10px;"> Grades </h1>
+        </div>
+
+        <div class = "OtherTile">
+            <img src="/GradeBook.png" alt="GradeBook" width=20px height=20px style="margin-tip: 10px;">
+            <h1 style="font-size: 10px;"> Grade Book </h1>
+        </div>
+
+        <div class = "OtherTile">
+            <img src="/Student.png" alt="Students" width=20px height=20px style="margin-tip: 10px;">
+            <h1 style="font-size: 10px;"> Students </h1>
+        </div>
+
+        <div class = "OtherTile">
+            <img src="/LightBulb.png" alt="Light Bulb Icon" width=20px height=20px style="margin-tip: 10px;">
+            <h1 style="font-size: 10px;"> Statements </h1>
+        </div>
+    </AppRailTile>
+</AppRail>
