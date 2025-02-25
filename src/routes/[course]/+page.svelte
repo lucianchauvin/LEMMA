d1e7fa2a6ee9e3ed0848880e35f0b4c54b319d05
Jared Wang <jwangj001@tamu.edu>
2025-02-25 02:59:09 -0600

    Used better images and modified the course page so that the labels are below the corresponding images

--- src/routes/[course]/+page.svelte
+++ src/routes/[course]/+page.svelte
@@ -1,37 +1,36 @@
 <script lang="ts">
-    import {AppRail, AppRailTile, AppRailAnchor} from '@skeletonlabs/skeleton';
-    let currentTile: number = 0;
+    import {AppRail, AppRailAnchor} from '@skeletonlabs/skeleton';
 </script>
 
 <AppRail>
     <svelte:fragment slot="lead">
         <AppRailAnchor href="">
             <img src="Hamburger_Icon.png" alt="Hamburger Icon" width=60px height=60px>
+            
         </AppRailAnchor>
     </svelte:fragment>
 
-    <AppRailAnchor href="/" title="Home">
-        <img src="/home-icon.svg" alt="Home" width="60px" height="60px">
-        <span>Home</span>
+    <AppRailAnchor href="/" title="Home" class="home-link">
+        <img src="/House_Icon.png" alt="Home" width="60px" height="60px" >
+        <div> Home </div>
     </AppRailAnchor>
 
     <AppRailAnchor href="/grades" title="Grades">
-        <img src="/CheckMark.jpg" alt="Grades" width="60px" height="60px">
-        <span>Grades</span>
+        <img src="/CheckMark.png" alt="Grades" width="60px" height="60px">
+        <div>Grades</div>
     </AppRailAnchor>
 
     <AppRailAnchor href="/gradebook" title="GradeBook">
         <img src="/GradeBook.png" alt="Grade Book" width="60px" height="60px">
-        <span>Grade Book</span>
+        <div>Grade Book</div>
     </AppRailAnchor>
 
     <AppRailAnchor href="/students" title="Students">
         <img src="/Student.png" alt="Students" width="60px" height="60px">
-        <span>Students</span>
     </AppRailAnchor>
 
     <AppRailAnchor href="/statements" title="Statements">
         <img src="/LightBulb.png" alt="Statements" width="60px" height="60px">
-        <span>Statements</span>
+        <div>Statements</div>
     </AppRailAnchor>
 </AppRail>
