80cfdbd42e0f8cacc1e75adbafc9f0e3b12deaab
Jared Wang <jwangj001@tamu.edu>
2025-02-24 21:34:40 -0600

    FrontEnd: Course Page - Finished the overall layout of the AppRail and its tiles

--- src/routes/[course]/+page.svelte
+++ src/routes/[course]/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <style>
-
+    
     .CourseNavigationTile {
         display: flex;
         justify-content: center;
@@ -13,7 +13,6 @@
         background-color: azure;
         border-radius: 8px;
     }
-
 
     .BarsSolid {
         display: flex;
@@ -40,6 +39,20 @@
         align-items: center;     
         justify-content: center; 
     }
+
+    .HomeTile img {
+        filter: grayscale(100%) brightness(0%);
+    }
+
+    .OtherTile {
+        width: 60px;
+        height: 60px;
+        background-color: azure;
+        display: flex;
+        flex-direction: column;  
+        align-items: center;     
+        justify-content: center; 
+    }
 </style>
 
 <AppRail>
@@ -55,6 +68,26 @@
         <div class="HomeTile">
             <img src="/home-icon.svg" alt="House" width=15px height=15px style="margin-top: 10px;"> 
             <h1 style="font-size: 10px;"> Home </h1>
+        </div>
+
+        <div class = "OtherTile">
+            <img src="/CheckMark.jpg" alt="Grades" width=20px height=20px style="margin-tip: 10px;">
+            <h1 style="font-size: 10px;"> Grades </h1>
+        </div>
+
+        <div class = "OtherTile">
+            <img src="/GradeBook.png" alt="GradeBook" width=20px height=20px style="margin-tip: 10px;">
+            <h1 style="font-size: 10px;"> Grade Book </h1>
+        </div>
+
+        <div class = "OtherTile">
+            <img src="/Student.png" alt="Students" width=20px height=20px style="margin-tip: 10px;">
+            <h1 style="font-size: 10px;"> Students </h1>
+        </div>
+
+        <div class = "OtherTile">
+            <img src="/LightBulb.png" alt="Light Bulb Icon" width=20px height=20px style="margin-tip: 10px;">
+            <h1 style="font-size: 10px;"> Statements </h1>
         </div>
     </AppRailTile>
 </AppRail>
