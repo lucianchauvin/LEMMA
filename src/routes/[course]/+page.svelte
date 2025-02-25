da1661c5ee6ead6edfb48c05addec82d6358ed68
Jared Wang <jwangj001@tamu.edu>
2025-02-24 18:52:00 -0600

    frontend: course page - Added the home tile on the AppRail

--- src/routes/[course]/+page.svelte
+++ src/routes/[course]/+page.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <style>
+
     .CourseNavigationTile {
         display: flex;
         justify-content: center;
@@ -28,6 +29,17 @@
         background-color: black;
         border-radius: 2px;
     }
+
+    .HomeTile {
+        width: 60px;
+        height: 60px;
+        background-color: green;
+        margin-top: 100px; 
+        display: flex;
+        flex-direction: column;  
+        align-items: center;     
+        justify-content: center; 
+    }
 </style>
 
 <AppRail>
@@ -38,6 +50,11 @@
                 <div></div>
                 <div></div>
             </div>
+        </div>
+
+        <div class="HomeTile">
+            <img src="/home-icon.svg" alt="House" width=15px height=15px style="margin-top: 10px;"> 
+            <h1 style="font-size: 10px;"> Home </h1>
         </div>
     </AppRailTile>
 </AppRail>
