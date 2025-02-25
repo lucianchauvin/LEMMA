39a9a6f9177dc4c025269fc68ce7883e9f662b3c
Marvin Fung <marvin.fung37@gmail.com>
2025-02-25 14:10:32 -0600

    Adds error checking when querying database on each page

--- src/routes/[course]/+page.server.ts
+++ src/routes/[course]/+page.server.ts
@@ -3,7 +3,19 @@ import type { Course } from '$lib/types';
 import { error } from '@sveltejs/kit';
 
 export const load = (async ({params, locals: { database }}) => {
-    const result = await database.query("SELECT * FROM courses WHERE course_id=$1", [params.course]);
+
+    let result;
+    try {
+        result = await database.query("SELECT * FROM courses WHERE course_id=$1", [params.course]);
+    } catch (err) {
+        console.error('Database failed to query for courses for specific course:', err);
+        error(500, {message: 'Database failed to query for courses for specific course'})
+    }
+
+    if(!result) {
+        console.error('Database failed to query for courses for specific course');
+        error(500, {message: 'Database failed to query for courses for specific course'})
+    }
 
     if (!result.rows) throw error(404); // no course found like this
     if (result.rows.length > 1) {
