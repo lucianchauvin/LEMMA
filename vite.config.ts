356df246beec0bfe557a7c008366dab07b9cb1c6
Lucian Chauvin <lucianchauvin@gmail.com>
2025-02-27 13:07:46 -0600

    LEAN: working! fixed build to run, dev still has an issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,42 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import importMetaUrlPlugin from '@codingame/esbuild-import-meta-url-plugin'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { normalizePath } from 'vite'
+import path from 'node:path'
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [importMetaUrlPlugin]
+        }
+    },
+    plugins: [
+        sveltekit(),
+        nodePolyfills({
+            overrides: {
+                fs: 'memfs',
+            },
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: [
+                        normalizePath(path.resolve(__dirname, './node_modules/lean4monaco/node_modules/@leanprover/infoview/dist/*')),
+                        normalizePath(path.resolve(__dirname, './node_modules/lean4monaco/dist/webview/webview.js')),
+                    ],
+                    dest: 'infoview'
+                },
+                {
+                    src: [
+                        normalizePath(path.resolve(__dirname, './node_modules/lean4monaco/node_modules/@leanprover/infoview/dist/codicon.ttf'))
+                    ],
+                    dest: 'assets'
+                }
+            ]
+        })
+    ],
     preview: {
         allowedHosts: ['lemma-lean.org'],
     },
