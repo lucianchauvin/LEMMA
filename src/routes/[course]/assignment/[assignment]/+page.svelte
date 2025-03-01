78336ce9d2a0366173c2a0de93d602b032457dea
Marvin Fung <marvin.fung37@gmail.com>
2025-03-01 00:21:57 -0600

    Change assignments page to use a components

--- src/routes/[course]/assignment/[assignment]/+page.svelte
+++ src/routes/[course]/assignment/[assignment]/+page.svelte
@@ -6,13 +6,26 @@
 
 <h1 class="h1">{data.assignment.name}</h1>
 
-<a href="{urlBase}/{data.edit.student_assignment_id}">Edit Assignment</a>
+{#if data.edit}
+<a href="{urlBase}/{data.edit.student_assignment_id}" class="btn variant-filled">Edit Assignment</a>
+{/if}
 
-<ul>
-    {#each data.students as student}
-        <li> 
-            <a href="{urlBase}/{student.student_assignment_id}">{student.username}</a>
-        </li>
-    {/each}
-</ul>
+<div class="table-container">
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>Student</th> 
+            <th>Grade</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each data.students as student}
+            <tr>
+                <td><a href="{urlBase}/{student.student_assignment_id}">{student.username}</a></td>
+                <td>{student.grade}</td>
+            </tr>
+        {/each}
+    </tbody>
 
+</table>
+</div>
