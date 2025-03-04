7c9d154ba17b6030451bd503b18c90b0cf6857fe
Marvin Fung <marvin.fung37@gmail.com>
2025-03-04 08:30:13 -0600

    Fix missing links for assignments on course page

--- src/routes/[course]/+page.svelte
+++ src/routes/[course]/+page.svelte
@@ -13,23 +13,27 @@
 <div class="table-wrapper pt-8 pr-3">
     <table class="table border border-gray-200 shadow-lg rounded-lg">
         <thead class="bg-gray-100">
-            <tr>
-                <th class="p-3 "><center> Assignment Name </center></th>
-                <th class="p-3 "><center> Assignment Description </center></th>
-                <th class="p-3 "><center> Due Date </center></th>
-                <th class="p-3 "><center> Open? </center></th>
+            <tr class="!text-center">
+                <th class="p-3"> Assignment Name </th>
+                <th class="p-3"> Assignment Description </th>
+                <th class="p-3"> Due Date </th>
+                <th class="p-3"> Open? </th>
             </tr>
         </thead>
         <tbody>
             {#each assignmentsData as assignments}
-                <tr class="border-t border-gray-300">
-                    <td class="p-3 {!assignments.active && 'bg-error-100'}"> <center> {assignments.assignment_name} </center> </td>
-                    <td class="p-3 {!assignments.active && 'bg-error-100'}"> <center> {assignments.assignment_description} </center></td>
-                    <td class="p-3 {!assignments.active && 'bg-error-100'}"> <center> {assignments.due_date.toLocaleDateString()} </center></td>
+                <tr class="border-t border-gray-300 text-center">
+                    <td class="p-3 {!assignments.active && 'bg-error-100'}"> 
+                        <a class="anchor" href="/{data.course.id}/assignment/{assignments.assignment_id}">
+                            {assignments.assignment_name}
+                        </a>
+                    </td>
+                    <td class="p-3 {!assignments.active && 'bg-error-100'}"> {assignments.assignment_description}</td>
+                    <td class="p-3 {!assignments.active && 'bg-error-100'}"> {assignments.due_date.toLocaleDateString()}</td>
                     {#if assignments.active === true}
-                        <td class="p-3"> <center>Yes</center> </td>
+                        <td class="p-3"> Yes </td>
                     {:else}
-                        <td class="p-3 bg-error-100"> <center>No</center> </td>
+                        <td class="p-3 bg-error-100"> No </td>
                     {/if}
                 </tr>
             {/each}
