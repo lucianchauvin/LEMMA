d51724b39c148dba25db64ba89a6f442c95ceb29
Andrew800 <andrewzhengschool@gmail.com>
2025-03-04 04:23:32 -0600

    Added statements page

--- src/routes/[course]/+layout.svelte
+++ src/routes/[course]/+layout.svelte
@@ -64,7 +64,7 @@
     </AppRailAnchor>
 </AppRail>
 
-<div class="body flex-1">
+<div class="body flex-1 mr-5">
     {#key data.url}
         <div in:fade|global={{ delay: 300, duration: 300 }} out:fade={{ duration: 300 }}>
             <slot/>
--- src/routes/[course]/+page.svelte
+++ src/routes/[course]/+page.svelte
@@ -4,9 +4,11 @@
     export let data;
 </script>
 
+<h2 class="h2 pb-3 ml-2 font-semibold border-b-2 border-surface-200">Course Assignments</h2>
+
 <p>{data.course.description}</p>
 
-<div class="table-wrapper pr-3">
+<div class="table-wrapper pt-8 pr-3">
     <table class="table border border-gray-200 shadow-lg rounded-lg">
         <thead class="bg-gray-100">
             <tr>
@@ -19,13 +21,13 @@
         <tbody>
             {#each assignmentsData as assignments}
                 <tr class="border-t border-gray-300">
-                    <td class="p-3"> <center> {assignments.assignment_name} </center> </td>
-                    <td class="p-3"> <center> {assignments.assignment_description} </center></td>
-                    <td class="p-3"> <center> {assignments.due_date.toLocaleDateString()} </center></td>
+                    <td class="p-3 {!assignments.active && 'bg-error-100'}"> <center> {assignments.assignment_name} </center> </td>
+                    <td class="p-3 {!assignments.active && 'bg-error-100'}"> <center> {assignments.assignment_description} </center></td>
+                    <td class="p-3 {!assignments.active && 'bg-error-100'}"> <center> {assignments.due_date.toLocaleDateString()} </center></td>
                     {#if assignments.active === true}
                         <td class="p-3"> <center>Yes</center> </td>
                     {:else}
-                        <td class="p-3 bg-error-400"> <center>No</center> </td>
+                        <td class="p-3 bg-error-100"> <center>No</center> </td>
                     {/if}
                 </tr>
             {/each}
