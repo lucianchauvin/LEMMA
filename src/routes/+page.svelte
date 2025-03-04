057e7b7bdc81e9a4b1e950dee84c8677d3bb9da0
Andrew800 <andrewzhengschool@gmail.com>
2025-03-03 22:27:45 -0600

    Changed CourseComponent to be a Card, added Home Page side bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,50 @@
 <script>
+    import { AppRail, AppRailAnchor } from '@skeletonlabs/skeleton';
+    import { Menu, House, CalendarDays, BookMarked, BookCheck, BookOpenCheck, Users, SquareFunction } from "lucide-svelte";
     import CourseComponent from '$lib/components/CourseComponent.svelte';
     import { fade, slide } from "svelte/transition"
     export let data;
 </script>
 
-<div class="left-side pr-5 float-left h-screen">
+<div class="all flex flex-wrap">
+<AppRail class="float-left -ml-5 mr-16 -mt-5 h-screen shadow-lg shadow-surface-900">
+    <AppRailAnchor>
+        <svelte:fragment slot="lead">
+            <Menu />
+        </svelte:fragment>
+    </AppRailAnchor>
+
+    <AppRailAnchor href="/" title="Home" class="home-link" selected={true}>
+        <svelte:fragment slot="lead">
+            <House />
+        </svelte:fragment>
+        <div>Home</div>
+    </AppRailAnchor>
+
+    <AppRailAnchor href="/" title="Calendar" class="calendar-link" selected={false}>
+        <svelte:fragment slot="lead">
+            <CalendarDays />
+        </svelte:fragment>
+        <div>Calendar</div>
+    </AppRailAnchor>
+</AppRail>
+
+<div class="left-side pr-5 min-w-[65%] max-w-[65%]">
     <h2 class="h2 pb-8 ml-2 font-semibold transition" in:fade|global={{ delay: 300, duration: 300 }}>Courses</h2>
 
     <div class="courses-container flex flex-wrap gap-20">
         {#each data.courses as { course_id, course_name, course_number, color }}
-                <button type="button" class="btn flex-none p-0 rounded-xl shadow-lg shadow-surface-500 shadow-lg shadow-surface-700 shadow-lg shadow-surface-900" in:slide|global={{ delay: 300, duration: 300 }}>
-                    <a href="/{course_id}">
-                        <CourseComponent CourseName={course_name} CourseNumber={course_number} CourseColor={color}/>
-                    </a>
-                </button> 
+            <CourseComponent {course_id} {course_name} {course_number} {color}/>
         {/each}
     </div>
 </div>
 
-<div class="right-side pl-5 h-screen border-l-2 border-surface-200 float-right" in:fade={{ delay: 300, duration: 300 }}>
-    <!-- <div class="right-side-upper border-b-2 border-surface-200" in:fade={{ delay: 300, duration: 300 }}>
-        <div class="calendar-container flex flex-wrap gap-20">
-            <h2 class="h2 pb-8 ml-2 font-semibold" in:fade|global={{ delay: 300, duration: 300 }}>Calendar Placeholder</h2>
-        </div>
-    </div> -->
+<div class="right-side pl-5 h-screen border-l-2 border-surface-200 " in:fade={{ delay: 300, duration: 300 }}>
     <div class="right-side-lower">
-        <div class="todo-container flex flex-wrap gap-20">
+        <div class="todo-container grid grid-cols-1 gap-5">
             <h2 class="h2 pb-8 ml-2 font-semibold" in:fade|global={{ delay: 300, duration: 300 }}>To Do Placeholder</h2>
+
         </div>
     </div>
+</div>
 </div>
--- src/routes/[course]/+layout.svelte
+++ src/routes/[course]/+layout.svelte
@@ -1,12 +1,12 @@
 <script>
     import { AppRail, AppRailAnchor } from '@skeletonlabs/skeleton';
-    import { Menu, House, BookMarked, BookCheck, BookOpenCheck, Users, SquareFunction } from "lucide-svelte";
+    import { Menu, House, CalendarDays, BookMarked, BookCheck, BookOpenCheck, Users, SquareFunction } from "lucide-svelte";
     import { fade } from "svelte/transition"
     import { page } from '$app/stores';
     export let data;
 </script>
 
-<AppRail class="float-left -ml-5 mr-10 -mt-5 h-screen shadow-lg shadow-surface-900">
+<AppRail class="float-left -ml-5 mr-16 -mt-5 h-screen shadow-lg shadow-surface-900">
     <AppRailAnchor>
         <svelte:fragment slot="lead">
             <Menu />
@@ -20,45 +20,52 @@
         <div>Home</div>
     </AppRailAnchor>
 
+    <AppRailAnchor href="/" title="Calendar" class="calendar-link" selected={$page.url.pathname === "/calendar"}>
+        <svelte:fragment slot="lead">
+            <CalendarDays />
+        </svelte:fragment>
+        <div>Calendar</div>
+    </AppRailAnchor>
+
     <AppRailAnchor href="/{data.course.course_id}" title="Assignments" class="assignment-link" selected={$page.url.pathname === `/${data.course.course_id}`}>
         <svelte:fragment slot="lead">
             <BookMarked />
         </svelte:fragment>
-        <div>Assignments</div>
+        <div>Course Assignments</div>
     </AppRailAnchor>
 
     <AppRailAnchor href="/{data.course.course_id}/grades" title="Grades" selected={$page.url.pathname === `/${data.course.course_id}/grades`}>
         <svelte:fragment slot="lead">
             <BookCheck />
         </svelte:fragment>
-        <div>Grades</div>
+        <div>Course Grades</div>
     </AppRailAnchor>
 
     <AppRailAnchor href="/{data.course.course_id}/gradebook" title="Gradebook" selected={$page.url.pathname === `/${data.course.course_id}/gradebook`}>
         <svelte:fragment slot="lead">
             <BookOpenCheck />
         </svelte:fragment>
-        <div>Gradebook</div>
+        <div>Course Gradebook</div>
     </AppRailAnchor>
 
     <AppRailAnchor href="/{data.course.course_id}/students" title="Students" selected={$page.url.pathname === `/${data.course.course_id}/students`}>
         <svelte:fragment slot="lead">
             <Users />
         </svelte:fragment>
-        <div>Students</div>
+        <div>Course Students</div>
     </AppRailAnchor>
 
     <AppRailAnchor href="/{data.course.course_id}/statements" title="Statements" selected={$page.url.pathname === `/${data.course.course_id}/statements`}>
         <svelte:fragment slot="lead">    
             <SquareFunction />
         </svelte:fragment>
-        <div>Statements</div>
+        <div>Course Statements</div>
     </AppRailAnchor>
 </AppRail>
 
 <div class="body">
     {#key data.url}
-        <div in:fade|global={{ delay: 300, duration: 300}} out:fade|global={{ duration: 300 }}>
+        <div in:fade|global={{ delay: 300, duration: 300 }} out:fade={{ duration: 300 }}>
             <slot/>
         </div>
     {/key}
