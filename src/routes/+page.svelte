0b645e32a4da124dbec65da45b6f5725441c7d89
Jared Wang <jwangj001@tamu.edu>
2025-03-07 22:31:20 -0600

    For the input below Enter your password, changed the input type to password so that user's input does not display characters and instead black dots

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,7 +30,7 @@
 
             <div>
                 <label> Password: </label>
-                <input type = "text" bind:value={password} placeholder="Enter your password" 
+                <input type = "password" bind:value={password} placeholder="Enter your password" 
                 class="w-full px-3 py-2 border rounded-md focus:ring-2 focus:ring-blue-500"/>
             </div>
 
