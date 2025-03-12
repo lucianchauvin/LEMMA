8e8ca94107db060f15907074efd21c6e1c5cdbe6
Jared Wang <jwangj001@tamu.edu>
2025-03-12 15:02:11 -0500

    Fixed the query error for UserRole - spelling error of the table name

--- src/routes/[course]/gradebook/+page.server.ts
+++ src/routes/[course]/gradebook/+page.server.ts
@@ -6,7 +6,7 @@ export const load = (async ({parent, params, locals: { safeQuery }}) => {
     const { course } = await parent();
     const {data: assignmentResult, error: assignmentErr} = await safeQuery<Assignment>("SELECT * FROM assignments WHERE assignments.course_id=$1", [params.course]);
     const {data: userResult, error: userErr } = await safeQuery<User>("SELECT * FROM users WHERE users.user_id=$1", [params.course ]);
-    const {data: userRoleResult, error: userRoleErr} = await safeQuery<UserRole>("SELECT * FROM user_roles WHERE users_role.user_id=$1", [params.course]); 
+    const {data: userRoleResult, error: userRoleErr} = await safeQuery<UserRole>("SELECT * FROM user_roles WHERE user_roles.user_id=$1", [params.course]); 
 
     if (assignmentErr)
     {
@@ -20,11 +20,11 @@ export const load = (async ({parent, params, locals: { safeQuery }}) => {
         error(500, {message: 'Database failed to query users for course'})
     }
     
-    // if (userRoleErr)
-    // {
-    //     console.error('ERROR: Database failed to query user roles for course:', userRoleErr);
-    //     error(500, {message: 'Database failed to query for user roles for course'})
-    // }
+    if (userRoleErr)
+    {
+        console.error('ERROR: Database failed to query user roles for course:', userRoleErr);
+        error(500, {message: 'Database failed to query for user roles for course'})
+    }
 
     return {
         title: `${course.course_number}: ${course.course_name}`,
