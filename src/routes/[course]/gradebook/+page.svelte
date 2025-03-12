5043f4db34fe82f5cb38212cd142afeb14b3054e
Jared Wang <jwangj001@tamu.edu>
2025-03-12 16:36:48 -0500

    Fixed queries for gradebook .ts page and completed the design for gradebook page

--- src/routes/[course]/gradebook/+page.svelte
+++ src/routes/[course]/gradebook/+page.svelte
@@ -5,12 +5,22 @@
     const usersRoleData = $page.data.user_roles ?? [];
     const usersData = $page.data.users ?? [];
     const assignmentsData = $page.data.assignments ?? [];
+    const studentAssignmentsData = $page.data.student_assignments ?? [];
     export let data;
     
     //filter users who are students
     const students = usersData.filter(user => {
         return usersRoleData.some(role => role.user_id === user.user_id && role.role_name === 'student')
     });
+
+    function fetchGrade(studentID, assignmentID)    {
+        const studentAssignment = studentAssignmentsData.find(sa => sa.student_id === studentID && sa.assignment_id === assignmentID);
+    
+        if (studentAssignment) {
+            return studentAssignment.grade;
+        } 
+        return '-';
+    }
 
     function determineLetterGrade(grades)   {
         if (grades.length === 0)    
@@ -39,12 +49,30 @@
     <table class="border-collapse border border-black w-full mt-4">
         <thead>
             <tr>
-                <th class = "p-3 text-center border-r border-black"> Student Name </th>
+                <th class = "p-3 text-center border border-black"> Student Name </th>
                 {#each assignmentsData as assignments}
-                    <th class = "p-3 text-center border-r border-black"> {assignments.assignment_name} </th>
+                    <th class = "p-3 text-center border border-black"> {assignments.assignment_name} </th>
                 {/each}
-                <th class="p-3 text-center border-r border-black">Letter Grade</th>
+                <th class="p-3 text-center border border-black">Letter Grade</th>
             </tr>
         </thead>
+        <tbody>
+            {#each students as student}
+                <tr>
+                    <td class="p-3 text-center border border-black">{student.first_name} {student.last_name}</td>
+                    {#each assignmentsData as assignments}
+                        <td class="p-3 text-center border border-black">
+                            {fetchGrade(student.user_id, assignments.assignment_id)}
+                        </td>
+                    {/each}
+                    <td class="p-3 text-center border border-black">
+                        {determineLetterGrade(
+                            assignmentsData.map(assignment => fetchGrade(student.user_id, assignment.assignment_id))
+                            .filter(grade => grade !== '-').map(Number)
+                        )}
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
     </table>
 </div>
