71439fcf58b4952fe8204f815cbc9d80612d42b1
Marvin Fung <marvin.fung37@gmail.com>
2025-03-18 12:01:21 -0500

    Merge editor to v2 skeleton

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,68 @@
 import { fileURLToPath, URL } from "node:url";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import importMetaUrlPlugin from '@codingame/esbuild-import-meta-url-plugin'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { normalizePath } from 'vite'
+import path from 'node:path'
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [importMetaUrlPlugin]
+        },
+        exclude: ['Projects']
+    },
+    kit: {
+        outDir: 'client/dist',
+    },
+    plugins: [
+        sveltekit(),
+        process.env.NODE_ENV === 'production' && nodePolyfills({
+          overrides: {
+            fs: 'memfs', // Use 'memfs' for the 'fs' module in development
+          },
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: [
+                        normalizePath(path.resolve(__dirname, './node_modules/@leanprover/infoview/dist/*')),
+                        normalizePath(path.resolve(__dirname, './node_modules/lean4monaco/dist/webview/webview.js')),
+                    ],
+                    dest: 'infoview'
+                },
+                {
+                    src: [
+                        normalizePath(path.resolve(__dirname, './node_modules/@leanprover/infoview/dist/codicon.ttf'))
+                    ],
+                    dest: 'assets'
+                }
+            ]
+        })
+    ],
     preview: {
         allowedHosts: ['lemma-lean.org'],
     },
     resolve: {
         alias: {
+            path: "path-browserify",
             '@lucide/svelte/icons': fileURLToPath(
                 new URL('./node_modules/@lucide/svelte/dist/icons', import.meta.url)
             )
         }
-    }
+    },
+    server: {
+        port: 3000,
+        proxy: {
+            '/websocket': {
+                target: 'ws://localhost:8080',
+                    ws: true
+            },
+            '/api': {
+                target: 'http://localhost:8080',
+            },
+        }
+    },
 });
