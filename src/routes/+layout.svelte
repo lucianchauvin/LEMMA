e2fb7e1bd4c1bfc6e6ce9bcce9a881d2540bdf09
Marfung37 <85657284+Marfung37@users.noreply.github.com>
2025-03-18 12:12:23 -0500

    Revert "Migration"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,30 @@
 <script>
     import "../app.css";
-    import { Avatar } from '@skeletonlabs/skeleton-svelte';
+    import { Avatar } from '@skeletonlabs/skeleton';
     import Triangle from "@lucide/svelte/icons/triangle";
 </script>
 
-<div class="h-screen flex flex-col w-full">
-
-<header class="header h-20 bg-surface-700 flex shadow-md shadow-surface-900">
-    <div class="home flex items-center gap-2">
-        <a href="/" class="btn bg-initial drop-shadow-xl">
-            <Triangle size=48 color="#57cfa7" />
-        </a>
-        <h1 class="h2 drop-shadow-xl font-medium text-primary-50">LEMMA</h1>
+<header class="header bg-surface-700 flex shadow-md shadow-surface-900">
+    <div class="home flex items-center">
+        <button type="button" class="btn bg-initial drop-shadow-xl">
+            <a href="/">
+                <Triangle size=48 color="#57cfa7" />
+            </a>
+        </button>
+        <h2 class="h2 drop-shadow-xl font-medium text-primary-50">LEMMA</h2>
     </div>
 
     <div class="admin flex items-center ml-auto mr-10">
-        <button type="button" class="btn btn-sm rounded-full bg-surface-50 preset-outlined-error-500 hover:preset-filled-error-500 shadow-lg shadow-surface-900 text-error-600">
+        <button type="button" class="btn btn-sm border-2 border-error-600 bg-surface-100 hover:variant-filled-error shadow-lg shadow-surface-900 text-error-600">
             <a href="/admin">Admin Panel</a>
         </button>
     </div>
 
     <div class="pfp flex items-center p-2">
-        <Avatar name="A Z" background="bg-secondary-200"/>
+        <Avatar initials="AZ" background="bg-secondary-200"/>
     </div>
 </header>
 
-<main class="flex-grow">
+<main class="p-5">
     <slot></slot>
 </main>
-</div>
