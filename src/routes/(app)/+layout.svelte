04e36ba088531d784dd045044e7578e8014a0bd4
Marvin Fung <marvin.fung37@gmail.com>
2025-03-20 04:47:58 -0500

    Add logout button to header and remove avatar

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -4,24 +4,28 @@
 </script>
 
 <div class="h-screen">
-<header class="header bg-surface-700 flex shadow-md shadow-surface-900">
+<header class="header bg-surface-700 p-2 flex justify-between shadow-md shadow-surface-900">
     <div class="home flex items-center">
-        <button type="button" class="btn bg-initial drop-shadow-xl">
+        <button type="button" class="btn bg-initial drop-shadow-xl text-primary-400">
             <a href="/">
-                <Triangle size=48 color="#57cfa7" />
+                <Triangle size=48 />
             </a>
         </button>
         <h2 class="h2 drop-shadow-xl font-medium text-primary-50">LEMMA</h2>
     </div>
 
-    <div class="admin flex items-center ml-auto mr-10">
-        <button type="button" class="btn btn-sm border-2 border-error-600 bg-surface-100 hover:variant-filled-error shadow-lg shadow-surface-900 text-error-600">
-            <a href="/admin">Admin Panel</a>
-        </button>
-    </div>
+    <div class="flex items-center gap-4">
+    <button id="admin" type="button" class="btn btn-sm border-2 border-error-600 bg-surface-100 hover:variant-filled-error shadow-lg shadow-surface-900 text-error-600">
+        <a href="/admin">Admin Panel</a>
+    </button>
 
-    <div class="pfp flex items-center p-2">
-        <Avatar initials="AZ" background="bg-secondary-200"/>
+    <!-- <div id="pfp"> -->
+    <!--     <Avatar initials="AZ" background="bg-secondary-200"/> -->
+    <!-- </div> -->
+
+    <form method="GET" action="/logout" data-sveltekit-reload>
+      <button id="logout" type="button" class="btn btn-sm border-2 border-surface-600 bg-surface-100 hover:variant-filled-surface shadow-lg shadow-surface-900 text-surface-600">Logout</button>
+    </form>
     </div>
 </header>
 
