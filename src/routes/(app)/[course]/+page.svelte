1b193bedbf22c0c5926c75680b16e46c653b5c59
Jared Wang <jwangj001@tamu.edu>
2025-03-20 20:25:14 -0500

    Forgot to handle a merge conflict in page.svelte in course directory

--- src/routes/(app)/[course]/+page.svelte
+++ src/routes/(app)/[course]/+page.svelte
@@ -10,19 +10,11 @@
 <div class="table-wrapper pt-8 pr-3">
     <table class="table border border-gray-200 shadow-lg rounded-lg">
         <thead class="bg-gray-100">
-<<<<<<< HEAD:src/routes/[course]/+page.svelte
-            <tr>
-                <th class="p-3 text-center"> Assignment Name </th>
-                <th class="p-3 text-center"> Assignment Description </th>
-                <th class="p-3 text-center"> Due Date </th>
-                <th class="p-3 text-center"> Open? </th>
-=======
             <tr class="!text-center">
                 <th class="p-3"> Assignment Name </th>
                 <th class="p-3"> Assignment Description </th>
                 <th class="p-3"> Due Date </th>
                 <th class="p-3"> Open? </th>
->>>>>>> main:src/routes/(app)/[course]/+page.svelte
             </tr>
         </thead>
         <tbody>
