9f3c99b715f8cd9340759218fcd677c0aa7eb944
Jared Wang <jwangj001@tamu.edu>
2025-03-21 10:42:06 -0500

    Removed succcessMessage variable.  Actually the code for page.svelte in login directory for previous commit was correct.  THe problem was that I was typing in the hashed password, as database actually contains hashed password instead of plaintext due to security reasons

--- src/routes/(app)/login/+page.svelte
+++ src/routes/(app)/login/+page.svelte
@@ -4,16 +4,12 @@
     import { onDestroy } from "svelte";
 
     let errorMessage = "";
-    let successMessage = "";
 
-    // Subscribe to form error and success messages
     const unsubscribe = page.subscribe(($page) => {
         errorMessage = $page.form?.message ?? "";
-        successMessage = $page.form?.success ? "Successfully logged in!" : "";
 
         if (errorMessage) {
-            // Display the alert with the error message
-            alert(errorMessage); // This will trigger the browser's native alert box
+            alert(errorMessage); 
         }
     });
 
@@ -24,10 +20,6 @@
 <div class="h-full flex justify-center mt-4">
 <div class="h-1/4 w-1/4 flex flex-col gap-2">
     <h1>Sign in</h1>
-        <!-- Display success message if the user logs in successfully -->
-        {#if successMessage}
-            <div class="success-message">{successMessage}</div>
-        {/if}
 
     <form method="post" class="flex flex-col gap-2" use:enhance>
         <label for="username">Username</label>
@@ -40,8 +32,3 @@
 </div>
 </div>
 
-<style>
-    .success-message {
-        color: green;
-    }
-</style>
