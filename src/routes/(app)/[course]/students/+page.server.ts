23e4d84ee3d7b939fb566bf8ac0d86330e83cd53
Jared Wang <jwangj001@tamu.edu>
2025-03-23 17:40:44 -0500

    Worked on the student's page as well

--- src/routes/(app)/[course]/students/+page.server.ts
+++ src/routes/(app)/[course]/students/+page.server.ts
@@ -1,1 +1,33 @@
+import type { PageServerLoad } from "../$types";
+import type { User, UserRole} from "$lib/types";
+import { error } from "@sveltejs/kit";
 
+export const load = (async ({ parent, params, locals: { safeQuery } }) => {
+    const { course } = await parent();
+    const { data: userResult, error: userErr } 
+    = await safeQuery<User>("SELECT u.* FROM users u JOIN user_roles ur ON u.user_id = ur.user_id WHERE ur.course_id = $1", [params.course]);
+    const {data: userRoleResult, error: userRoleErr} 
+    = await safeQuery<UserRole>("SELECT ur.*, r.display_name FROM user_roles ur JOIN roles r ON ur.role_name = r.role_name WHERE ur.course_id = $1", [params.course]); 
+
+    if (userErr) {
+        console.error("ERROR: Database failed to query users for course:", userErr);
+        error(500, { message: "Database failed to query users for course" });
+    }
+
+    if (userRoleErr) {
+        console.error("ERROR: Database failed to query user roles for course:", userRoleErr);
+        error(500, { message: "Database failed to query for user roles for course" });  
+    }
+
+    return {
+        title: `${course.number}: ${course.name}`,
+        course: {
+            id: course.id, 
+            name: course.name,
+            number: course.number,
+            description: course.description,
+        },
+        users: userResult,
+        user_roles: userRoleResult,
+    };
+}) satisfies PageServerLoad;
---
+++ src/routes/(app)/[course]/students/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import {page} from '$app/stores';
+    import Users from '@lucide/svelte/icons/users'; // Importing the Users icon
+    const usersRoleData = $page.data.user_roles ?? [];
+    const usersData = $page.data.users ?? [];
+    export let data;
+
+    // Filter users who are students
+    const students = usersData.filter(user => {
+        return usersRoleData.some(role => role.user_id === user.user_id && role.role_name === 'student')
+    });
+</script>
+
+<div class ="table-container">
+    <h1 class="text-xl font-bold flex items-center gap-2">
+        <Users size={24} /> Students
+    </h1>
+
+    <table class="table table-hover mt-4">
+        <thead>
+            <tr>
+                <th class="p-3 text-center">Student Name</th>
+                <th class="p-3 text-center">Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each students as student}
+                <tr class="text-center">
+                    <td>{student.first_name} {student.last_name}</td>
+                    <td>{student.email}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
