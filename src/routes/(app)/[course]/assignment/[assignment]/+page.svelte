90e04d106c475b7ab8d04f81c993b81d24430265
Marvin Fung <marvin.fung37@gmail.com>
2025-04-19 16:27:45 -0500

    Change the assignment page to be a data table also

--- src/routes/(app)/[course]/assignment/[assignment]/+page.svelte
+++ src/routes/(app)/[course]/assignment/[assignment]/+page.svelte
@@ -1,31 +1,47 @@
 <script lang="ts">
     export let data;
 
+    import DatatableClient from '$lib/components/client/Datatable.svelte';
+    import Users from '@lucide/svelte/icons/users';
+
     const urlBase = `/${data.course.course_id}/assignment/${data.assignment.assignment_id}`;
+
+    let editing: {id: string, col: string} | null = null;
+
+    const columnConfig = {
+      username: {
+        render: (row) => ({
+          element: 'a',
+          props: {
+            class: "anchor",
+            href: `${urlBase}/{student.student_assignment_id}`,
+          },
+          children: row.username ?? 'No student name found'
+        }),
+      },
+    }
+
 </script>
 
-<h1 class="h1">{data.assignment.name}</h1>
+<div class="flex flex-col gap-4">
+<h1 class="h1 text-3xl font-bold flex items-center gap-2">
+    <Users size={30} /> 
+    <p>Student Assignments</p>
+</h1>
+<hr>
 
 {#if data.edit}
 <a href="{urlBase}/{data.edit.student_assignment_id}" class="btn variant-filled">Edit Assignment</a>
 {/if}
 
-<div class="table-container">
-<table class="table table-hover">
-    <thead>
-        <tr>
-            <th>Student</th> 
-            <th>Grade</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each data.students as student}
-            <tr>
-                <td><a href="{urlBase}/{student.student_assignment_id}">{student.username}</a></td>
-                <td>{student.grade}</td>
-            </tr>
-        {/each}
-    </tbody>
-
-</table>
+<DatatableClient rowsPerPage={10} data={data.students ?? []} columns={["username", "grade"]} display_columns={["Student", "Grade"]}>
+    <svelte:fragment slot="cell" let:row let:col>
+    {@const display = columnConfig[col]?.render(row) ?? {element: 'span', children: row[col]}}
+    <div class="w-full flex justify-between">
+    <svelte:element this={display.element} {...display.props}>
+      {display.children}
+    </svelte:element>
+    </div>
+    </svelte:fragment>
+</DatatableClient>
 </div>
