1190c7c5c3b8ada203d01eb0fa9b17526ffd1100
Andrew800 <andrewzhengschool@gmail.com>
2025-04-29 02:54:17 -0500

    fix typo in comment for config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry tests 10 times */
+  /* Retry tests 5 times */
   retries: 5,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
